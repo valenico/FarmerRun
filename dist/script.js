469a8e76be0a104ac7626d8630cc84b966b75ac2
Valerio <valerio.nicolanti@hotmail.it>
2019-07-30 15:56:00 +0200

    new model

--- dist/script.js
+++ dist/script.js
@@ -35,8 +35,18 @@ window.addEventListener( 'resize', function () {
 
 document.body.appendChild( renderer.domElement);
 
+/*////////////////////////////////////////////////////////////////////
+
+
+var qInitial = new THREE.Quaternion().setFromAxisAngle( (0,0,1) , -5 );
+var qFinal = new THREE.Quaternion().setFromAxisAngle( (0,0,1),5);
+var quaternionKF = new THREE.QuaternionKeyframeTrack( '.quaternion', [ 0, 1, 2 ], [ qInitial.x, qInitial.y, qInitial.z, qInitial.w, qFinal.x, qFinal.y, qFinal.z, qFinal.w, qInitial.x, qInitial.y, qInitial.z, qInitial.w ] );
+var clip = new THREE.AnimationClip( 'Run', 3, [quaternionKF] );
+*/
+
 function renderScene(){ renderer.render( scene, camera ); }
 renderCalls.push(renderScene);
+
 
 /* ////////////////////////////////////////////////////////////////////////// */
 
@@ -74,18 +84,26 @@ scene.add( light );
 
 var loader = new THREE.GLTFLoader();
 loader.crossOrigin = true;
-loader.load( './../models/scene.gltf', function ( data ) {
+loader.load( './../models/eggman-yurro.glb', function ( data ) {
 
   
     var object = data.scene;
     object.position.set(0, 0, -0.75);
-    object.scale.set(2,2,2);
+    //object.scale.set(2,2,2);
+  
 
     data.scene.traverse(function(child){
       console.log(child);
+      if(child.name == "Thigh_R_014"){
+        //child.rotateZ(5);
+      }
      });
+    //var mixer = new THREE.AnimationMixer( object );
+    //var clipAction = AnimationAction(mixer ,clip );
+    //clipAction.play(object);
 
     //object.position.y = - 95;
+    var animations = data.animations;
+    console.log(animations);
     scene.add( object );
-  //, onProgress, onError );
 });
