81ede3585c79791d41f69960cae8b73408dbb56d
Valerio <valerio.nicolanti@hotmail.it>
2019-08-17 13:05:52 +0200

    rings generations functions put in another file

---
+++ dist/script(1).js
@@ -0,0 +1,172 @@
+
+const backgroundColor = 0x000000;
+
+/*////////////////////////////////////////*/
+
+var renderCalls = [];
+function render () {
+  requestAnimationFrame( render );
+  renderCalls.forEach((callback)=>{ callback(); });
+}
+// render();
+
+/*////////////////////////////////////////*/
+
+var scene = new THREE.Scene();
+
+var camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 800 );
+camera.position.set(0,2,-4);
+
+var renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor( backgroundColor );//0x );
+
+renderer.toneMapping = THREE.LinearToneMapping;
+renderer.toneMappingExposure = Math.pow( 0.94, 5.0 );
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
+
+window.addEventListener( 'resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}, false );
+
+document.body.appendChild( renderer.domElement);
+
+/*////////////////////////////////////////////////////////////////////
+
+
+var qInitial = new THREE.Quaternion().setFromAxisAngle( (0,0,1) , -5 );
+var qFinal = new THREE.Quaternion().setFromAxisAngle( (0,0,1),5);
+var quaternionKF = new THREE.QuaternionKeyframeTrack( '.quaternion', [ 0, 1, 2 ], [ qInitial.x, qInitial.y, qInitial.z, qInitial.w, qFinal.x, qFinal.y, qFinal.z, qFinal.w, qInitial.x, qInitial.y, qInitial.z, qInitial.w ] );
+var clip = new THREE.AnimationClip( 'Run', 3, [quaternionKF] );
+*/
+
+function renderScene(){ renderer.render( scene, camera ); }
+renderCalls.push(renderScene);
+
+
+/* ////////////////////////////////////////////////////////////////////////// */
+
+var controls = new THREE.OrbitControls( camera );
+
+controls.rotateSpeed = 0.3;
+controls.zoomSpeed = 0.9;
+
+controls.minDistance = 3;
+controls.maxDistance = 20;
+
+controls.minPolarAngle = 0; // radians
+controls.maxPolarAngle = Math.PI /2; // radians
+
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+
+renderCalls.push(function(){
+  controls.update()
+});
+
+
+/* ////////////////////////////////////////////////////////////////////////// */
+
+
+
+var light = new THREE.AmbientLight( 0x20202A, 20, 100 );
+light.position.set( 30, -10, 30 );
+scene.add( light );
+
+/* ////////////////////////////////////////////////////////////////////////// */
+
+var moverGroup = new THREE.Object3D();
+scene.add( moverGroup );
+var floorGroup = new THREE.Object3D();
+
+var floorMaterial = new THREE.MeshLambertMaterial({
+	color: 0x006400, //diffuse							
+	emissive: 0x000000, 
+	shading: THREE.FlatShading, 
+	side: THREE.DoubleSide,
+	map: new THREE.TextureLoader().load('./ground.jpg'),
+});
+
+floorGeometry = new THREE.PlaneGeometry( 20, 1000 , 20,20 );
+var floorMesh = new THREE.Mesh( floorGeometry, floorMaterial );
+
+floorGroup.add( floorMesh );
+moverGroup.add( floorGroup );
+floorMesh.rotation.x = Math.PI/2;
+//floorMesh.rotation.z = Math.PI/2;
+floorGroup.position.y = 0;
+moverGroup.position.z = 0;
+floorGroup.position.z = 500;
+
+
+/*//////////////////////////////////////////*/
+
+var loader = new THREE.GLTFLoader();
+loader.crossOrigin = true;
+
+loader.load( './../models/scene.gltf', function ( gltf ) {
+
+  
+    var sonic = gltf.scene;
+    sonic.position.set(0, 0, -0.75);
+    //object.scale.set(2,2,2);
+  
+
+    gltf.scene.traverse(function(child){
+      //console.log(child);
+      if(child.name == "Thigh_R_014"){
+        //child.rotateZ(5);
+      }
+     });
+    //var mixer = new THREE.AnimationMixer( object );
+    //var clipAction = AnimationAction(mixer ,clip );
+    //clipAction.play(object);
+
+    //object.position.y = - 95;
+    scene.add( sonic );
+    animate(sonic);
+});
+
+function animate(sonic){
+	console.log(sonic.position);
+	//scene.remove(sonic);
+
+}
+
+// Metodo con cui carica la texture
+// bisogna capire come farla vedere bene
+
+var onLoad = function (texture) {
+  var objGeometry = new THREE.PlaneGeometry(100, 100, 32);
+  var objMaterial = new THREE.MeshPhongMaterial({
+    map: texture,
+    side: THREE.DoubleSide,
+    shading: THREE.FlatShading
+  });
+
+  var mesh = new THREE.Mesh(objGeometry, objMaterial);
+  scene.add(mesh);
+  mesh.rotation.x = 300;
+}
+
+// Function called when download progresses
+var onProgress = function (xhr) {
+  console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+};
+
+// Function called when download errors
+var onError = function (xhr) {
+  console.log('An error happened');
+};
+
+
+
+var loader1 = new THREE.TextureLoader();
+loader1.load('./ground.jpg', onLoad, onProgress, onError);
+
+// fine metodo texture
+render();
--- dist/script.js
+++ dist/script.js
@@ -1,139 +1,335 @@
-
 const backgroundColor = 0x000000;
+
+/// Dictionary for body parts of sonic ///
+const sonic_dic = { 
+
+  Testa : "Head_06",
+
+  Fianchi : "Hips_05",
+  Bacino_dx : "Pelvis_R_013",
+  Bacino_sx : "Pelvis_L_08",
+
+  Coscia_dx : "Thigh_R_014",
+  Polpaccio_dx : "Calf_R_015",
+  Piede_dx : "Foot_R_016",
+
+  Coscia_sx : "Thigh_L_09",
+  Polpaccio_sx : "Calf_L_010",
+  Piede_sx : "Foot_L_011",
+
+  Spalla_dx : "Shulder_R_034",
+  Braccio_dx : "UpperArm_R_035",
+  Avambraccio_dx : "ForeArm_R_036",
+  Polso_dx : "Wrist_R_048",
+  Mano_dx : "Hand_R_037",
+
+  Spalla_sx : "Shulder_L_0019",
+  Braccio_sx : "UpperArm_L_020",
+  Avambraccio_sx : "ForeArm_L_021",
+  Polso_sx : "Wrist_L_033",
+  Mano_sx : "Hand_L_022"
+
+};
+
+/// AXIS (+)
+
+// x = laterale sx
+// y = su
+// z = avanti
 
 /*////////////////////////////////////////*/
 
-var renderCalls = [];
+/*var renderCalls = []; 
 function render () {
   requestAnimationFrame( render );
   renderCalls.forEach((callback)=>{ callback(); });
 }
-// render();
-
+*/ 
 /*////////////////////////////////////////*/
+var renderer, scene, camera, controls, sonic, ring, ring2;
+var t = 0;
+var jump = false;
+var score = 0;
+//function renderScene(){ renderer.render( scene, camera ); }
+//renderCalls.push(renderScene);
 
-var scene = new THREE.Scene();
+var max_distance = 50;
+var min_space = 2;
+var probability = 0.3;
+var rings = new Array();
+var size = 5;
+var lines_type = new Array(5);
 
-var camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 800 );
-camera.position.set(0,2,-4);
+function init() {
 
-var renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setClearColor( backgroundColor );//0x );
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 800 );
+  camera.position.set(0,2,-4);
 
-renderer.toneMapping = THREE.LinearToneMapping;
-renderer.toneMappingExposure = Math.pow( 0.94, 5.0 );
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = THREE.PCFShadowMap;
-
-window.addEventListener( 'resize', function () {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( window.innerWidth, window.innerHeight );
-}, false );
+  renderer.setClearColor( backgroundColor );//0x );
 
-document.body.appendChild( renderer.domElement);
+  renderer.toneMapping = THREE.LinearToneMapping;
+  renderer.toneMappingExposure = Math.pow( 0.94, 5.0 );
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFShadowMap;
 
-/*////////////////////////////////////////////////////////////////////
+  window.addEventListener( 'resize', function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+  }, false );
 
+  document.body.appendChild( renderer.domElement);
 
-var qInitial = new THREE.Quaternion().setFromAxisAngle( (0,0,1) , -5 );
-var qFinal = new THREE.Quaternion().setFromAxisAngle( (0,0,1),5);
-var quaternionKF = new THREE.QuaternionKeyframeTrack( '.quaternion', [ 0, 1, 2 ], [ qInitial.x, qInitial.y, qInitial.z, qInitial.w, qFinal.x, qFinal.y, qFinal.z, qFinal.w, qInitial.x, qInitial.y, qInitial.z, qInitial.w ] );
-var clip = new THREE.AnimationClip( 'Run', 3, [quaternionKF] );
-*/
+  controls = new THREE.OrbitControls( camera );
 
-function renderScene(){ renderer.render( scene, camera ); }
-renderCalls.push(renderScene);
+  controls.rotateSpeed = 0.3;
+  controls.zoomSpeed = 0.9;
 
+  controls.minDistance = 3;
+  controls.maxDistance = 20;
 
-/* ////////////////////////////////////////////////////////////////////////// */
+  controls.minPolarAngle = 0; // radians
+  controls.maxPolarAngle = Math.PI /2; // radians
 
-var controls = new THREE.OrbitControls( camera );
+  controls.enableDamping = true;
+  controls.dampingFactor = 0.05;
 
-controls.rotateSpeed = 0.3;
-controls.zoomSpeed = 0.9;
+ /* renderCalls.push(function(){
+    controls.update()
+  }); */
 
-controls.minDistance = 3;
-controls.maxDistance = 20;
+  document.addEventListener("keydown", onDocumentKeyDown, false);
+  function onDocumentKeyDown(event) {
+    var keyCode = event.which;
+    if (keyCode == 65 && sonic.position.x < 4) {
+      sonic.position.x += 0.5;
+    } else if (keyCode == 68 && sonic.position.x > -4) {
+      sonic.position.x -= 0.5;
+    } else if (keyCode == 32){
+      jump = true;
+    }
+  };
+  var light = new THREE.AmbientLight( 0x20202A, 20, 100 );
+  light.position.set( 30, -10, 30 );
+  scene.add( light );
 
-controls.minPolarAngle = 0; // radians
-controls.maxPolarAngle = Math.PI /2; // radians
-
-controls.enableDamping = true;
-controls.dampingFactor = 0.05;
-
-renderCalls.push(function(){
-  controls.update()
-});
-
-
-/* ////////////////////////////////////////////////////////////////////////// */
-
-
-
-var light = new THREE.AmbientLight( 0x20202A, 20, 100 );
-light.position.set( 30, -10, 30 );
-scene.add( light );
-
-/* ////////////////////////////////////////////////////////////////////////// */
-
-var moverGroup = new THREE.Object3D();
-scene.add( moverGroup );
-var floorGroup = new THREE.Object3D();
-
-var floorMaterial = new THREE.MeshLambertMaterial({
-	color: 0x006400, //diffuse							
-	emissive: 0x000000, 
-	shading: THREE.FlatShading, 
-	side: THREE.DoubleSide,
-	map: new THREE.TextureLoader().load('./ground.jpg'),
-});
-
-floorGeometry = new THREE.PlaneGeometry( 20, 1000 , 20,20 );
-var floorMesh = new THREE.Mesh( floorGeometry, floorMaterial );
-
-floorGroup.add( floorMesh );
-moverGroup.add( floorGroup );
-floorMesh.rotation.x = Math.PI/2;
-//floorMesh.rotation.z = Math.PI/2;
-floorGroup.position.y = 0;
-moverGroup.position.z = 0;
-floorGroup.position.z = 500;
-
-
-/*//////////////////////////////////////////*/
+}
 
 var loader = new THREE.GLTFLoader();
 loader.crossOrigin = true;
 
 loader.load( './../models/scene.gltf', function ( gltf ) {
 
-  
-    var sonic = gltf.scene;
+    sonic = gltf.scene;
     sonic.position.set(0, 0, -0.75);
     //object.scale.set(2,2,2);
   
-
-    gltf.scene.traverse(function(child){
-      //console.log(child);
-      if(child.name == "Thigh_R_014"){
-        //child.rotateZ(5);
+  /*  gltf.scene.traverse(function(child){
+      if(child.name == 'Shoulder_L_Reference'){
+        child.rotateZ(-0.8);
+        child.rotateX(1);
+        child.rotateY(-1);
       }
-     });
-    //var mixer = new THREE.AnimationMixer( object );
-    //var clipAction = AnimationAction(mixer ,clip );
-    //clipAction.play(object);
-
-    //object.position.y = - 95;
+     }); */
+    sonic.name = "sonic"
     scene.add( sonic );
-    animate(sonic);
+   // animate(sonic);
+
+
 });
 
-function animate(sonic){
-	console.log(sonic.position);
-	//scene.remove(sonic);
+loader.load( './../models/eggman-yurro.glb', function ( gltf ) {
+  eggman = gltf.scene;
+  eggman.position.set(0, 0, -100);
+  eggman.scale.set(0.5,0.5,0.5);
+  scene.add( eggman );
+});
+
+loader.load('./../models/ring.glb', function(gltf) {
+    var ring = gltf.scene;
+    ring.scale.set(0.005,0.005,0.005);
+    randomCoinInitialization(ring);
+});
+
+
+function lerp1(current, target, fraction){
+  return (target-current)*fraction;
+}
+
+function lerp(current, target, fraction){
+  var array_of_points = [];
+
+  for (var is = 0; is < (1/fraction); is++){
+    var j = is*fraction;
+    array_of_points.push(current*(1-j)+target*j); 
+  }
+
+  return array_of_points;
+}
+
+
+// for the z position of sonic i just use a scalar, we don't have a 'ending' point
+s = 0.02;
+
+// the array is fucking ordered! leg dx up, low -- leg sx up, low
+run_speed = 0.03;
+run = [lerp(6, 8 , run_speed).concat(lerp(8, 6, run_speed)), lerp(4, 5.5, run_speed).concat(lerp(5.5,4,run_speed)), 
+       lerp(8, 6, run_speed).concat(lerp(6,8,run_speed)),lerp(5.5 , 4, run_speed).concat(lerp(4,5.5,run_speed)),
+       lerp(0, -2, run_speed).concat(lerp(-2,0,run_speed)),lerp(-2, 0, run_speed).concat(lerp(0,-2,run_speed)),
+       lerp(0, 1, run_speed).concat(lerp(1,0,run_speed)),lerp(1, 0, run_speed).concat(lerp(0,1,run_speed))];
+
+
+jump_points = lerp(0 , 1.5 , 0.04).concat(lerp(1.5 , 0 , 0.04));
+var t_jump = 0;
+
+eggman_moves_x = lerp( 0 , -2.5, run_speed/6).concat(lerp(-2.5,2.5,run_speed/3)).concat(lerp(2.5,0,run_speed/6));
+var t_egg = 0;
+egg_speed = 0.06;
+var egg = true;
+var is_time = false;
+var hitting = false;
+var t_hit = 0;
+var n_hit = 0;
+var egglight = new THREE.PointLight('red', 100);
+
+error = 0.5;
+
+function check_ring(){
+  var res;
+  var group;
+  for(var i = 0; i < rings.length ; i++){
+      r = rings[i];
+      var id = i%size;
+      var parent = i - id;
+      group = Math.floor(i/size);
+
+      if(scene.getObjectById(r.id) == null){
+        ringRepositioning(r, parent, id);
+        continue;
+      }
+      try {
+        if(sonic.position.z >= r.position.z + 2) ringRepositioning(r, parent, id, group); // ring miss
+
+        z = sonic.position.z <= r.position.z + error;
+        z1 = sonic.position.z >= r.position.z - error;
+
+        y = sonic.position.y <= r.position.y + error;
+        y2 = sonic.position.y >= r.position.y - error;
+
+        x = sonic.position.x <= r.position.x + error;
+        x2 = sonic.position.x >= r.position.x - error;
+        if(z && z1 && x && x2 && y && y2){ 
+          res = r;
+          break;
+        }
+        else res = -1;
+      } catch(err){}
+    }
+
+  if(res != -1){
+      score += 10;
+      scene.remove(res);
+      var i = rings.indexOf(r);
+      var id = i%size;
+      var parent = i - id;
+      ringRepositioning(r, parent, id, group);
+      //rings[r].position.set( (Math.random() < 0.5 ? -1 : 1)*Math.random()*3 , Math.random() < 0.5 ? 1.2 : 0.5, sonic.position.z + Math.random()*10+2);
+    }
+}
+
+var text2 = document.createElement('h1');
+text2.style.position = 'absolute';
+text2.style.color = "white";
+text2.innerHTML = score;
+text2.style.top = 50 + 'px';
+text2.style.left = 50 + 'px';
+document.body.appendChild(text2);
+
+function animate(){
+
+  object = scene.getObjectByName( "sonic", true );
+  if(typeof(object)!= "undefined"){
+  //if(t >= 0.5) s.getObjectByName(sonic_dic.Testa).rotation.x += 1;
+  //t = (t >= 1) ? 0 : t+= 0.002;
+
+    sonic.position.z += lerp1(0, 10, 0.01);
+    camera.position.z += lerp1(0, 10, 0.01);
+  
+    sonic.getObjectByName(sonic_dic.Polpaccio_dx).rotation.z = run[1][t];
+    sonic.getObjectByName(sonic_dic.Coscia_dx).rotation.z = run[0][t];
+      
+    sonic.getObjectByName(sonic_dic.Polpaccio_sx).rotation.z = run[3][t];
+    sonic.getObjectByName(sonic_dic.Coscia_sx).rotation.z = run[2][t];
+
+    sonic.getObjectByName(sonic_dic.Avambraccio_sx).rotation.z = run[4][t];
+    sonic.getObjectByName(sonic_dic.Avambraccio_dx).rotation.z = run[5][t];
+
+    sonic.getObjectByName(sonic_dic.Braccio_dx).rotation.z = run[6][t];
+    sonic.getObjectByName(sonic_dic.Braccio_sx).rotation.z = run[7][t];
+    sonic.getObjectByName(sonic_dic.Braccio_dx).rotation.y = -1.5;
+    sonic.getObjectByName(sonic_dic.Braccio_sx).rotation.y = 1.5;
+    sonic.getObjectByName(sonic_dic.Testa).rotation.z = 0.2;
+      
+    if(egg){
+      is_time = Math.random() > 0.99;
+      if(is_time){
+        egg = false;
+        eggman.position.z = sonic.position.z - 5;
+  egglight.position.set(eggman.position.x, eggman.position.y, eggman.position.z + 2.5);
+  scene.add(egglight);
+      }
+    }
+
+    if(is_time){
+      eggman.position.x = eggman_moves_x[t_egg]
+      
+      //here that should be a random movement towards sonic to hit him
+      if(eggman.position.z >= sonic.position.z + 4){ //we're in front of sonic
+        hitting = Math.random() > 0.9; //probability of trying to hit
+      } else if(hitting == false){
+        eggman.position.z += egg_speed;
+      }
+
+      if(hitting){
+        eggman.position.z -= (n_hit == 10) ? 0.07 : 0.04; //going back towards sonic, full speed if it's last hit
+        t_hit +=1;
+        if(t_hit >= 80){ // after going to him, we set time and variable, he'll go back to its z position
+          t_hit = 0;
+          n_hit +=1;
+          hitting = false;
+        }
+      }
+
+      t_egg = (t_egg >= eggman_moves_x.length) ? 0 : t_egg+1;
+
+      if(n_hit == 11){
+        eggman.position.z -= 0.07; //smooth disappearing, otherwise it just stops
+        is_time = false;
+  n_hit = 0;
+        egg = true;
+      }
+    } 
+    t = (t == run[0].length) ? 0 : t+=1;
+    
+    check_ring();
+    
+    text2.innerHTML = score;
+    egglight.position.set(eggman.position.x, eggman.position.y, eggman.position.z + 2.5);
+  }
+  if(jump){
+    sonic.position.y = jump_points[t_jump];
+    t_jump += 1;
+    if(t_jump >= jump_points.length){
+      jump = false;
+      t_jump = 0;
+    }
+  }
+  requestAnimationFrame(animate);
+  render();
 
 }
 
@@ -141,7 +337,17 @@ function animate(sonic){
 // bisogna capire come farla vedere bene
 
 var onLoad = function (texture) {
-  var objGeometry = new THREE.PlaneGeometry(100, 100, 32);
+
+  texture.wrapS = THREE.RepeatWrapping;
+  texture.wrapT = THREE.RepeatWrapping;
+
+  const times_horizontal = 3;
+  const times_vert = 500;
+
+  texture.repeat.set(times_horizontal, times_vert);
+
+  var objGeometry = new THREE.PlaneGeometry(10, 1000);
+
   var objMaterial = new THREE.MeshPhongMaterial({
     map: texture,
     side: THREE.DoubleSide,
@@ -150,12 +356,12 @@ var onLoad = function (texture) {
 
   var mesh = new THREE.Mesh(objGeometry, objMaterial);
   scene.add(mesh);
-  mesh.rotation.x = 300;
+  mesh.rotation.x = 300.0222;
 }
 
 // Function called when download progresses
 var onProgress = function (xhr) {
-  console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+//console.log((xhr.loaded / xhr.total * 100) + '% loaded');
 };
 
 // Function called when download errors
@@ -163,10 +369,13 @@ var onError = function (xhr) {
   console.log('An error happened');
 };
 
-
-
 var loader1 = new THREE.TextureLoader();
-loader1.load('./ground.jpg', onLoad, onProgress, onError);
+loader1.load('./ground1.jpg', onLoad, onProgress, onError);
+
+function render(){
+  renderer.render(scene, camera);
+}
 
 // fine metodo texture
-render();
+init();
+animate();
