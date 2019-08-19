aa3e2a1ed0df70d7cb82488847547a846f0a53d1
Giammi <33184864+theroggio@users.noreply.github.com>
2019-08-19 17:45:19 +0200

    fixed check over columns

--- dist/obstacles.js
+++ dist/obstacles.js
@@ -12,24 +12,24 @@ var cylinder = new THREE.Mesh( cgeometry, cmaterial );
 function generate_obstacle(){  
     for(; n_obs <= max_obs; ){
         var pz = Math.random()*d;
-        var r = Math.random() > 0.5 ? cylinder.rotation.z : 80.1;
+        var or = Math.random() > 0.5 ? cylinder.rotation.z : 80.1;
         var px = (Math.random() > 0.5 ? -1 : 1)*2.5*Math.random();
         var put = true; 
         for(var i = 0; i < rings.length; i++){
-            var z1 = pz >= rings[i].position.z + 2; 
-            var z2 = pz <= rings[i].position.z - 2;
-            var x1 = px <= rings[i].position.x - 3;
-            var x2 = px >= rings[i].position.x + 3;
+            var z1 = pz <= rings[i].position.z + 2; 
+            var z2 = pz >= rings[i].position.z - 2;
+            var x1 = px >= rings[i].position.x - 3;
+            var x2 = px <= rings[i].position.x + 3;
             if(z1 && z2 && x1 && x2){
                 put = false;
                 break;
             }
         } 
         for(var l = 0; l < n_obs; l++){
-            var z1 = pz >= obs[l].position.z + 2; 
-            var z2 = pz <= obs[l].position.z - 2;
-            var x1 = px <= obs[l].position.x - 3;
-            var x2 = px >= obs[l].position.x + 3;
+            var z1 = pz <= obs[l].position.z + 2; 
+            var z2 = pz >= obs[l].position.z - 2;
+            var x1 = px >= obs[l].position.x - 3;
+            var x2 = px <= obs[l].position.x + 3;
             if(z1 && z2 && x1 && x2){
                 put = false;
                 break;
@@ -37,8 +37,8 @@ function generate_obstacle(){
         }
         if(put){
             c = cylinder.clone();
-            c.rotation.z = r;
-            c.position.set(px, r == cylinder.rotation.z ? 1.25 : 0.25 , pz);
+            c.rotation.z = or;
+            c.position.set(px, or == cylinder.rotation.z ? 1.25 : 0.25 , pz);
             obs.push(c);
             scene.add(c);
             n_obs++;
@@ -48,32 +48,32 @@ function generate_obstacle(){
 
 function repositioningObstacle(index, from){
     var pz = Math.random()*d + from + min_dist;
-    var r = Math.random() > 0.5 ? cylinder.rotation.z : 80.1;
+    var or = Math.random() > 0.5 ? cylinder.rotation.z : 80.1;
     var px = (Math.random() > 0.5 ? -1 : 1)*Math.random()*2.5;
     var put= true;
     for(var i = 0; i < rings.length; i++){
-        var z1 = pz >= rings[i].position.z + 2; 
-        var z2 = pz <= rings[i].position.z - 2;
-        var x1 = px <= rings[i].position.x - 3;
-        var x2 = px >= rings[i].position.x + 3;
+        var z1 = pz <= rings[i].position.z + 2; 
+        var z2 = pz >= rings[i].position.z - 2;
+        var x1 = px >= rings[i].position.x - 3;
+        var x2 = px <= rings[i].position.x + 3;
         if(z1 && z2 && x1 && x2){
             put = false;
             break;
         }
     }
     for(var l = 0; l < n_obs; l++){
-        var z1 = pz >= obs[l].position.z + 2; 
-        var z2 = pz <= obs[l].position.z - 2;
-        var x1 = px <= obs[l].position.x - 3;
-        var x2 = px >= obs[l].position.x + 3;
+        var z1 = pz <= obs[l].position.z + 2; 
+        var z2 = pz >= obs[l].position.z - 2;
+        var x1 = px >= obs[l].position.x - 3;
+        var x2 = px <= obs[l].position.x + 3;
         if(z1 && z2 && x1 && x2){
             put = false;
             break;
         }
     }
     if(put){
-        obs[index].rotation.z = r;
-        obs[index].position.set(px, r == 80.1 ? 0.25 : 1.25, pz);
+        obs[index].rotation.z = or;
+        obs[index].position.set(px, or == 80.1 ? 0.25 : 1.25, pz);
     } 
 }
 
