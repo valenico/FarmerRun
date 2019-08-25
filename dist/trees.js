c6061386519139e9c592013b8d98d2153ff351a1
Giammi <33184864+theroggio@users.noreply.github.com>
2019-08-25 12:28:30 +0200

    fix tree generation random

--- dist/trees.js
+++ dist/trees.js
@@ -1,4 +1,4 @@
-var tree_types = new Array();
+var tree1, tree2, tree3, tree4;
 var trees = new Array();
 var max_trees = 10;
 var tree_dist = 50;
@@ -8,7 +8,9 @@ var objGeometry = new THREE.PlaneGeometry( 3 , 3 , 32);
 var times_horizontal = 1;
 var times_vert = 1;
 
-function tree_load(texture){
+var p = 0;
+
+loader1.load('./../Images/tree.png', function(texture){
     texture.repeat.set(times_horizontal, times_vert);
     var objMaterial = new THREE.MeshPhongMaterial({
         map: texture,
@@ -16,19 +18,47 @@ function tree_load(texture){
         shading: THREE.FlatShading,
         transparent: true,
       });
-    tree_types.push(new THREE.Mesh(objGeometry, objMaterial));
-    if(tree_types.length > 3) treesInit();
-}
+    tree1 = new THREE.Mesh(objGeometry, objMaterial);
+});
 
-loader1.load('./../Images/tree.png', tree_load);
-loader1.load('./../Images/tree1.png', tree_load);
-loader1.load('./../Images/tree4.png', tree_load);
-loader1.load('./../Images/tree5.png', tree_load);
+loader1.load('./../Images/tree1.png', function(texture){
+    texture.repeat.set(times_horizontal, times_vert);
+    var objMaterial = new THREE.MeshPhongMaterial({
+        map: texture,
+        side: THREE.DoubleSide,
+        shading: THREE.FlatShading,
+        transparent: true,
+      });
+    tree2 = new THREE.Mesh(objGeometry, objMaterial);
+});
+
+loader1.load('./../Images/tree4.png', function(texture){
+    texture.repeat.set(times_horizontal, times_vert);
+    var objMaterial = new THREE.MeshPhongMaterial({
+        map: texture,
+        side: THREE.DoubleSide,
+        shading: THREE.FlatShading,
+        transparent: true,
+      });
+    tree3 = new THREE.Mesh(objGeometry, objMaterial);
+});
+
+loader1.load('./../Images/tree5.png', function(texture){
+    texture.repeat.set(times_horizontal, times_vert);
+    var objMaterial = new THREE.MeshPhongMaterial({
+        map: texture,
+        side: THREE.DoubleSide,
+        shading: THREE.FlatShading,
+        transparent: true,
+      });
+    tree4 = new THREE.Mesh(objGeometry, objMaterial);
+    treesInit();
+});
 
 function treesInit(){
     for(var m = 0; m < max_trees;){
         var road_side = (Math.random() > 0.5 ? -1 : 1);
-        var px = road_side*Math.random()*10 +(road_side == -1 ? -3 : 3) ;
+        var px = road_side*Math.random()*10 +(road_side == -1 ? -4 : 4);
         var pz = Math.random()*tree_dist + 10;
         var put = true;
         for(var l = 0; l < trees.length; l++){
@@ -43,8 +73,20 @@ function treesInit(){
         }
 
         if(put){ 
-            var c = tree_types[Math.floor(Math.random()*tree_types.length)].clone();
-            c.position.set(px, 0 , pz);
+            if( p == 0 ){
+                c = tree1.clone();
+                p++;
+            } else if( p == 1 ){
+                c = tree2.clone();
+                p++;
+            } else if( p == 2 ){
+                c= tree3.clone();
+                p++;
+            } else {
+                c = tree4.clone();
+                p = 0;
+            }
+            c.position.set(px, 1 , pz);
             trees.push(c);
             scene.add(c);
             m++;
@@ -54,9 +96,9 @@ function treesInit(){
 
 function treesRepositioning(start){
     for(var i = 0; i < trees.length; i++){
-        if(trees[i].position.z +3 < sonic.position.z ){
+        if(trees[i].position.z + 2 < sonic.position.z ){
             var road_side = (Math.random() > 0.5 ? -1 : 1);
-            var px = road_side*Math.random()*10 +(road_side == -1 ? -3 : 3) ;
+            var px = road_side*Math.random()*10 +(road_side == -1 ? -4 : 4) ;
             var pz = Math.random()*tree_dist + start + min_tree_dist;
             var put = true;
             for(var l = 0; l < trees.length; l++){
@@ -71,7 +113,24 @@ function treesRepositioning(start){
             }
 
             if(put){
-                trees[i].position.set(px,0,pz);
+                scene.remove(trees[i]);
+                trees.splice(i,1);
+                if( p == 0 ){
+                    c = tree1.clone();
+                    p++;
+                } else if( p == 1 ){
+                    c = tree2.clone();
+                    p++;
+                } else if( p == 2 ){
+                    c= tree3.clone();
+                    p++;
+                } else {
+                    c = tree4.clone();
+                    p = 0;
+                }
+                c.position.set(px, 1 , pz);
+                trees.push(c);
+                scene.add(c);
             } 
         }
     }
